5a542b93e098d5b84c10c7ba5c1ce57787bcaf07
mateuszkornecki <mateusz.kornecki@gmail.com>
2019-12-15 16:06:52 +0100

    refactor: extracted form functions to utils.js

---
+++ src/js/utils.js
@@ -0,0 +1,24 @@
+const loading = document.querySelector('.js-loading');
+const successMessage = document.querySelector('.js-success-message');
+const errorMessage = document.querySelector('.js-error-message');
+
+function showLoadingIndicator(form) {
+        form.classList.add('is-hidden');
+        loading.classList.remove('is-hidden');
+}
+function showSuccessMessage(form, response) {
+        console.log('Success!', response);
+        setTimeout(() => {
+                successMessage.classList.remove('is-hidden');
+                loading.classList.add('is-hidden');
+        }, 500);
+}
+function showErrorMessage(form, error) {
+        console.error('Error!', error.message);
+        setTimeout(() => {
+                errorMessage.classList.remove('is-hidden');
+                loading.classList.add('is-hidden');
+        }, 500);
+}
+
+export { showLoadingIndicator, showSuccessMessage, showErrorMessage };
